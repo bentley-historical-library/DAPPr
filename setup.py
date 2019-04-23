97c0fae04a3e1585d1aecf487dbd3a3e9dec89d8
Dallas <djpillen@umich.edu>
2019-04-23 10:01:45 -0400

    add include_package_data parameter to setup function

--- setup.py
+++ setup.py
@@ -6,6 +6,7 @@ setuptools.setup(
     author="Bentley Historical Library",
     description="DSpace API Python Programming Language resource",
     packages=setuptools.find_packages(),
+    include_package_data=True,
     install_requires=[
         "requests",
         "humanize"
